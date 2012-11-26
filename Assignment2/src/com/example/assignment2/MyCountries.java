10ffb446359b3bc75c811bbe07ef9a4b37d713ac
Carl Bjerggaard <carl.bjerggaard@gmail.com>
2012-11-27 00:07:05 +0100

    messing up with arrayadapter and now moving over to customadapter

--- Assignment2/src/com/example/assignment2/MyCountries.java
+++ Assignment2/src/com/example/assignment2/MyCountries.java
@@ -2,6 +2,8 @@ package com.example.assignment2;
 
 import java.util.List;
 
+import com.example.assignment2.R.color;
+
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
@@ -36,84 +38,81 @@ public class MyCountries extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.mycountries_main_layout);
 
-		countryListView = (ListView) findViewById(R.id.country_list);
-
 		datasource = new CountriesDataSource(this);
 		datasource.open();
 
-		checkPreferences();
-
-		listAdapter = new ArrayAdapter<Country>(this,
-				R.layout.mycountries_row_layout, R.id.list_text_black, values);
-
-		countryListView.setAdapter(listAdapter);
-
-		PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
-
-		// fill ListView with elements
-		// ListView list = getListView();
-
-		// listAdapter = new ArrayAdapter<Country>(this,
-		// R.layout.mycountries_main_layout, R.id.list_text_blacker, values);
-		// setListAdapter(listAdapter);
-
-		registerForContextMenu(countryListView);
+		// checkPreferences();
+		//
+		// registerForContextMenu(countryListView);
 
 		// Use actionBar
 		ActionBar actionBar = getActionBar();
 		actionBar.setDisplayHomeAsUpEnabled(true);
 	}
 
-	private void checkPreferences() {
-		SharedPreferences prefs = PreferenceManager
-				.getDefaultSharedPreferences(this);
+	private void setupAdapter(int list_id) {
+		listAdapter = new ArrayAdapter<Country>(this,
+				R.layout.mycountries_row_layout, list_id, values);
 
-		// Load simple properties
-		sortProp = prefs.getInt("sortProp", -1);
-		checkboxPref = prefs.getBoolean("checkboxPref", false);
-		textColorPref = prefs.getString("textColorPref", "#C0C0C0");
-		backgroundColorPref = prefs.getString("backgroundColorPref",
-				"#00FF00FF");
-		editPref = prefs.getString("editPref", "NULL");
+		countryListView = (ListView) findViewById(R.id.country_list);
+		countryListView.setAdapter(listAdapter);
+		registerForContextMenu(countryListView);
+	}
 
-		// setup ListView adapter and fill ListView with elements
-		int list_text_color = R.id.list_text_black;
+	private void handleBackgroundColorProperties() {
+		// Handle background color prefs
+		View mainView = findViewById(R.id.country_main_layout);
+		mainView.setBackgroundColor(Color.parseColor(backgroundColorPref));
+	}
 
-		if (textColorPref.equals("red")) {
-			// showToast("textColorPref = red");
-			// list_text_color = R.id.list_text_red;
-		} else if (textColorPref.equals("green")) {
-			showToast("textColorPref = green");
-			// list_text_color = R.id.list_text_green;
-		} else {
-			showToast("jajaja " + textColorPref);
+	private int handleTextColorProperties() {
+		int list_text_color = -1;
+
+		showToast("textColorPref set to: " + textColorPref);
+
+		
+		if (textColorPref.equals("#FFFFFF")) { 
+			list_text_color = R.id.list_text_white;
+		} else if (textColorPref.equals("#FFFF00")) {
+			list_text_color = R.id.list_text_yellow;
+		} else if (textColorPref.equals("#FF00FF")) {
+			list_text_color = R.id.list_text_fuchsia;
+		} else if (textColorPref.equals("#FF0000")) {
+			list_text_color = R.id.list_text_red;
+		} else if (textColorPref.equals("#C0C0C0")) {
+			list_text_color = R.id.list_text_silver;
+		} else if (textColorPref.equals("#808080")) {
+			list_text_color = R.id.list_text_gray;
+		} else if (textColorPref.equals("#808000")) {
+			list_text_color = R.id.list_text_olive;
+		} else if (textColorPref.equals("#800080")) {
+			list_text_color = R.id.list_text_purple;
+		} else if (textColorPref.equals("#800000")) {
+			list_text_color = R.id.list_text_maroon;
+		} else if (textColorPref.equals("#00FFFF")) {
+			list_text_color = R.id.list_text_aqua;
+		} else if (textColorPref.equals("#00FF00")) {
+			list_text_color = R.id.list_text_lime;
+		} else if (textColorPref.equals("#008080")) {
+			list_text_color = R.id.list_text_teal;
+		} else if (textColorPref.equals("#008000")) {
 			list_text_color = R.id.list_text_green;
+		} else if (textColorPref.equals("#0000FF")) {
+			list_text_color = R.id.list_text_blue;
+		} else if (textColorPref.equals("#000080")) {
+			list_text_color = R.id.list_text_navy;
+		} else if (textColorPref.equals("#000000")) {
+			list_text_color = R.id.list_text_black;
+		} else {
+			list_text_color = R.id.list_text_black;
+			
 		}
 
-		// listAdapter = new ArrayAdapter<Country>(this,
-		// R.layout.mycountries_list_layout, list_text_color, values);
-		// setListAdapter(listAdapter);
+		showToast("list_text_color set to: " + list_text_color);
+		return list_text_color;
+	}
 
-		View mainView = findViewById(R.id.country_main_layout);
-
-		mainView.setBackgroundColor(Color.parseColor(backgroundColorPref));
-		// TextView text = (TextView) findViewById(R.id.list_text_black);
-		// text.setTextColor(getResources().getColor(R.color.maroon));
-
-		// listAdapter = new ArrayAdapter<Country>(this,
-		// R.layout.mycountries_list_layout, R.id.list_text_green, values);
-		// ListView listView = (ListView) findViewById(R.id.myList);
-		// ArrayAdapter<Country> adapter = new ArrayAdapter<Country>(this,
-		// R.layout.mycountries_list_layout, R.id.list_text_green, values);
-
-		// listView.setAdapter(adapter);
-		// setListAdapter(listAdapter);
-		// if (getListView() == null) {
-		// Log.i("calle", "listView == NULL");
-		//
-		// }
-
-		// get all countries
+	private void handleSortProperties() {
 		switch (sortProp) {
 		case 0:
 			values = datasource.getAllCountriesSortedByYearASC();
@@ -136,10 +135,28 @@ public class MyCountries extends Activity {
 			// showToast("values sorted by default");
 			break;
 		}
+	}
 
-		// showToast("sortProp : " + sortProp + "\ncheckboxPref: " +
-		// checkboxPref
-		// + "\ncolorPref: " + colorPref + "\neditPref: " + editPref);
+	private void checkPreferences() {
+		PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+
+		loadSimpleProperties();
+
+		handleSortProperties();
+
+		handleBackgroundColorProperties();
+
+		setupAdapter(handleTextColorProperties());
+	}
+
+	private void loadSimpleProperties() {
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		checkboxPref = prefs.getBoolean("checkboxPref", false);
+		sortProp = prefs.getInt("sortProp", -1);
+		backgroundColorPref = prefs.getString("backgroundColorPref", "NULL");
+		textColorPref = prefs.getString("textColorPref", "NULL");
+		editPref = prefs.getString("editPref", "NULL");
 	}
 
 	@Override
@@ -261,7 +278,7 @@ public class MyCountries extends Activity {
 		// showToast("onResume");
 		super.onResume();
 		datasource.open();
-		 checkPreferences();
+		checkPreferences();
 	}
 
 	@Override
